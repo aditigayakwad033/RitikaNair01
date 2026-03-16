0d6cdc8907838925539b65b81c78967846ed0f64
aditigayakwad033 <aditigayakwad033@zohomail.in>
2026-03-16 15:49:06 +0530

    Add files via upload
    Added test script 3 and 4 for log in module by ritika

---
+++ src/test/java/Test/NewTest3.java
@@ -0,0 +1,18 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest3 {
+	
+	
+  @Test
+  public void sf() {
+	 
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+	  
+  }
+}
